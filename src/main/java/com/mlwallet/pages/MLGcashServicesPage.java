f36f7ca5479300ead9f43f628671ec25641f0194
Jovanie Dasian <DASIAN@MLHUILLIER.LOCAL>
2023-09-27 14:06:02 +0800

    Initial Locator Template for Ml Gcash

---
+++ src/main/java/com/mlwallet/pages/MLGcashServicesPage.java
@@ -0,0 +1,25 @@
+package com.mlwallet.pages;
+
+import org.openqa.selenium.By;
+
+public class MLGcashServicesPage {
+    public static By objGcash = By.xpath("");
+    public static By objGcashOptionsPage = By.xpath("");
+    public static By objSendMoney = By.xpath("");
+    public static By objRecieverModal = By.xpath("");
+    public static By objMobileNumberInput = By.xpath("");
+    public static By objSelectFromContacts = By.xpath("");
+    public static By objAllow= By.xpath("");
+    public static By objDontAllow = By.xpath("");
+    public static By objSearchContact = By.xpath("");
+    public static By objContactsPage = By.xpath("");
+    public static By objContacts = By.xpath("");
+    public static By objFavorites = By.xpath("");
+    public static By objNextbtn = By.xpath("");
+    public static By objGcashMobileNumber = By.xpath("");
+    public static By objAmountToSend = By.xpath("");
+    public static By objSendMoneyToGcashPage = By.xpath("");
+    public static By objTransactionDetails = By.xpath("");
+    public static By objConfirmbtn = By.xpath("");
+
+}
